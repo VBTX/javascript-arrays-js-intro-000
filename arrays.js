c2393460c75c15fa45aa7d4289ff96bfdc3c7546
Victoria B <viktorija.bourassa@gmail.com>
2018-10-05 21:35:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,8 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
   chocolateBars.unshift(candyString);
   return chocolateBars;
 }
+function removeElementFromEndOfArray(){
+  var colors = ['black', 'blue', 'grey', 'red'];
+  colors.slice(0, colors.length-1);
+  colors;
+}
