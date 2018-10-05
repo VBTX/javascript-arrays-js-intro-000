41acedc3c2a6c96c5b72f76cfb57d40cd12bc3dc
Victoria B <viktorija.bourassa@gmail.com>
2018-10-05 21:13:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,5 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
 
-var chocolateBars;
-chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(){
   const colors = ['black', 'blue', 'grey'];
