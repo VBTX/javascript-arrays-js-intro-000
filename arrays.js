53505e76848a90a1ce041dc72d51ae9d98a147c1
Victoria B <viktorija.bourassa@gmail.com>
2018-10-05 21:27:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,9 @@
 
 
 var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles"];
+
+
+function destructivelyAddElementToBeginningOfArray(){
+  var colors = ['black', 'blue', 'grey'];
+  colors.unshift('red');
+}
