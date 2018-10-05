59ba246e63c6714de7b190ba1046745786624d0a
Victoria B <viktorija.bourassa@gmail.com>
2018-10-05 21:34:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,8 +9,3 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
   chocolateBars.unshift(candyString);
   return chocolateBars;
 }
-
-function addElementToBeginningOfArray(array, element){
-  var try = [1, 2, 3, 4];
-  try = [element,...try];
-}
