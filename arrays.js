6cd95ea1d96e0173cc852827d4727a74daca7f3f
Victoria B <viktorija.bourassa@gmail.com>
2018-10-05 21:26:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,51 @@
 
 
 var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles"];
+
+
+
+function addElementToBeginningOfArray(array, element){
+return [element, ...array];
+}
+
+
+function destructivelyAddElementToBeginningOfArray(){
+  var colors = ['black', 'blue', 'grey'];
+  colors.unshift('red');
+}
+
+function addElementToEndOfArray(){
+  const colors = ['black', 'blue', 'grey'];
+  const addition = [..colors, 'red'];
+}
+function destructivelyAddElementToEndOfArray(){
+  var colors = ['black', 'blue', 'grey'];
+  colors.push('red');
+}
+
+function iKnow(){
+  var things = ['everything', 'nothing', 'something'];
+  things[1];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(){
+  const colors = ['black', 'blue', 'grey', 'red'];
+  colors.shift();
+  colors;
+}
+
+function removeElementFromBeginningOfArray(){
+  var colors = ['black', 'blue', 'grey', 'red'];
+  colors.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(){
+    var colors = ['black', 'blue', 'grey', 'red'];
+    colors.pop();
+    colors;
+}
+function removeElementFromEndOfArray(){
+  var colors = ['black', 'blue', 'grey', 'red'];
+  colors.slice(0, colors.length-1);
+  colors;
+}
